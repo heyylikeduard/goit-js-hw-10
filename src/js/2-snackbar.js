305153010ce77d4e107053d7c1df0660d1619a67
heyylikeduard <edosheyylik@gmail.com>
2024-11-01 18:26:32 +0200

    add task 2

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,42 @@
+// Описаний у документації
+import iziToast from "izitoast";
+// Додатковий імпорт стилів
+import "izitoast/dist/css/iziToast.min.css";
+
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', function(event) {
+  event.preventDefault(); 
+
+  const delay = Number(form.delay.value); 
+  const state = form.state.value; 
+
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+
+
+  promise
+    .then((delay) => {
+      iziToast.success({
+        title: 'Success',
+        message: `✅ Fulfilled promise in ${delay}ms`,
+        position: 'topRight',
+      });
+    })
+    .catch((delay) => {
+      iziToast.error({
+        title: 'Error',
+        message: `❌ Rejected promise in ${delay}ms`,
+        position: 'topRight',
+      });
+    });
+});
